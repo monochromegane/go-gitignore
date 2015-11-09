49fcb58f3df323e1c9c76d6c20d01c9cf334a6cd
monochromegane <dev.kuro.obi@gmail.com>
2015-11-09 22:16:27 +0900

    Turned up by introducing simple matcher.

--- pattern.go
+++ pattern.go
@@ -10,10 +10,10 @@ var Separator = string(filepath.Separator)
 type pattern struct {
 	path          string
 	base          string
-	matchingPath  string
 	hasRootPrefix bool
 	hasDirSuffix  bool
 	pathDepth     int
+	matcher       pathMatcher
 }
 
 func newPattern(path, base string) pattern {
@@ -29,13 +29,19 @@ func newPattern(path, base string) pattern {
 		pathDepth = strings.Count(path, "/")
 	}
 
+	var matcher pathMatcher
+	if hasMeta(path) {
+		matcher = filepathMatcher{path: matchingPath}
+	} else {
+		matcher = simpleMatcher{path: matchingPath}
+	}
+
 	return pattern{
-		path:          path,
 		base:          base,
-		matchingPath:  matchingPath,
 		hasRootPrefix: hasRootPrefix,
 		hasDirSuffix:  hasDirSuffix,
 		pathDepth:     pathDepth,
+		matcher:       matcher,
 	}
 }
 
@@ -45,15 +51,15 @@ func (p pattern) match(path string, isDir bool) bool {
 		return false
 	}
 
-	var match bool
+	var targetPath string
 	if p.hasRootPrefix {
 		//absolute pattern
-		match, _ = filepath.Match(p.matchingPath, path)
+		targetPath = path
 	} else {
 		// relative pattern
-		match, _ = filepath.Match(p.matchingPath, p.equalizeDepth(path))
+		targetPath = p.equalizeDepth(path)
 	}
-	return match
+	return p.matcher.match(targetPath)
 }
 
 func (p pattern) equalizeDepth(path string) string {
@@ -64,4 +70,8 @@ func (p pattern) equalizeDepth(path string) string {
 		start = diff
 	}
 	return filepath.Join(strings.Split(trimedPath, Separator)[start:]...)
+}
+
+func hasMeta(path string) bool {
+	return strings.IndexAny(path, "*?[") >= 0
 }
